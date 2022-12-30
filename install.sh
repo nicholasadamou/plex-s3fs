d3b479738d8ff4224c1a41d3d065ceaf48b08357
Nicholas Adamou <nicholasadamouemail@gmail.com>
2022-12-30 08:01:17 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ main() {
 	ask "Enter Amazon AWS Secret Key: " ; secret_key=$(get_answer)
 
 	# Write Amazon AWS Secret Key & ID to '/etc/passwd-s3fs'.
-	echo "${secret_key_id}:${secret_key}" > "/etc/passwd-s3fs"
+	sudo echo "${secret_key_id}:${secret_key}" > "/etc/passwd-s3fs"
 
 	# Change the permissions of '/etc/passwd-s3fs'.
 	sudo chmod 600 "/etc/passwd-s3fs"
@@ -47,11 +47,11 @@ main() {
 	# Make the Amazon AWS S3 Bucket mount on boot.
 	payload="s3fs#${bucket_name} /mnt/${bucket_name} fuse _netdev,rw,nosuid,nodev,allow_other,nonempty 0 0"
 	grep -Fq "${payload}" /etc/fstab || {
-		echo "${payload}" >> /etc/fstab
+		sudo echo "${payload}" >> /etc/fstab
 	}
 	
 	# Mount the S3 Bucket
-	mount -a
+	sudo mount -a
 
 	print_warning "We need to obtain the required claim token. To do this, you will need to use ssh tunneling to gain access and setup the server for first run. During the first run, you setup Plex to make it available and configurable. However, this setup option will only be triggered if you access it over http://localhost:32400/web, it will not be triggered if you access it over http://ip_of_server:32400/web. Since we are setting up PMS (Plex Media Server) on a headless server, you can use a SSH tunnel to link http://localhost:32400/web (on your current computer) to http://localhost:32400/web (on the headless server running PMS) by executing: ssh username@ip_of_server -L 32400:localhost:32400 -N"
 
